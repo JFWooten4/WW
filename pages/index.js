0df56c47cd5ba1fa61b49a5bf03d3a3d297a3842
John Wooten <john@wootenwealth.com>
2024-01-29 17:18:11 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   useEffect(() => {
     const redirectTimeout = setTimeout(() => {
       router.push("https://www.youtube.com/@JFWooten4");
-    }, 10);
+    }, 0);
     return () => clearTimeout(redirectTimeout);
   }, []);
   
