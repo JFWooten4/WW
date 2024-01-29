ee51e201059baccc196b46c350d26d24afcf3bd5
John Wooten <john@wootenwealth.com>
2024-01-29 17:11:51 -0500

    Remove optin page_

--- pages/index.js
+++ pages/index.js
@@ -4,39 +4,10 @@ import Footer from "@components/Footer";
 
 export default function Home() {
   const router = useRouter();
-  
   useEffect(() => {
     const redirectTimeout = setTimeout(() => {
       router.push("https://www.youtube.com/@JFWooten4");
-    }, 100);
+    }, 0);
     return () => clearTimeout(redirectTimeout);
   }, []);
-  
-  return (
-    <div className="container" style={{ backgroundColor: "#201F1D" }}>
-      <Head>
-        <title>Wooten Wealth</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        {/* Add Image with marginTop */}
-        <img
-          src="imgs/smse-desktop.png"
-          alt="Stock Market Secrets Exposed"
-          style={{ maxWidth: "500px", height: "auto", marginTop: "35px" }} // Add marginTop
-        />
-
-        {/* Embed Zoho Form */}
-        <iframe
-          aria-label="Wooten Wealth Pre-Launch Signups"
-          frameBorder="0"
-          style={{ height: "600px", width: "99%", border: "none" }}
-          src="https://forms.zohopublic.com/blocktransfer/form/WootenWealthPreLaunchSignups/formperma/93cnN8rejRNSy4wtlV7CELjldUtFNpkG-PYGBwrfBK4"
-        ></iframe>
-      </main>
-
-      <Footer />
-    </div>
-  );
 }
